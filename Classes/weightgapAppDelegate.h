94ea01216a5a89267f98ff3f9e47e980d8beb270
bookchiq <sarah@happyhumans.com>
2011-02-20 21:20:36 -0700

    Initial phonegap project

---
+++ Classes/weightgapAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  weightgapAppDelegate.h
+//  weightgap
+//
+//  Created by Sarah Lewis on 2/20/11.
+//  Copyright Abundant Media, Inc. 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface weightgapAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
